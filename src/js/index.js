8ad896cdfa8377eb048d7f1a0c5f30be04efb88f
JH <vvaa277@gmail.com>
2023-05-08 13:40:14 +0900

    Fix: index.js 연결 파일 변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
 import { initModal } from '../js/events/initModal.js';
 import { initHeroSlide } from '../js/events/initHeroSlide.js';
 
-
 import { SearchBar } from "../js/events/searchBarTemp/SearchBarTemp.js";
 import { SearchPanelView } from "./events/searchBarTemp/SearchPanelView.js";
 import { SearchBarView } from "../js/events/searchBarTemp/SearchBarView.js";
 import { SearchBarModel } from "./events/searchBarTemp/SearchBarModel.js";
 
-import { SideBar, SideBarView } from "../js/events/sideBarTemp/SideBarTemp.js";
-import { SideBarModel } from "../js/events/sideBarTemp/SideBarModel.js";
+// import { SideBar, SideBarView } from "../js/events/sideBarTemp/SideBarTemp.js";
+// import { SideBarModel } from "../js/events/sideBarTemp/SideBarModel.js";
+
+import { SideBar } from "../js/events/sideBar/SideBar.js";
 
 const init = async () => {
   initHeroSlide();
@@ -19,10 +20,13 @@ const init = async () => {
   const searchBar = new SearchBar(searchBarModel, searchBarView, searchPanelView);
   searchBar.init()
 
-  const sideBarModel = new SideBarModel();
-  const sideBarview = new SideBarView();
-  const sideBar = new SideBar(sideBarModel, sideBarview);
-  sideBar.init()
+  const sideBar = new SideBar();
+  sideBar.initSideBar();
+
+  // const sideBarModel = new SideBarModel();
+  // const sideBarview = new SideBarView();
+  // const sideBar = new SideBar(sideBarModel, sideBarview);
+  // sideBar.init()
 
   await initModal();
 };
